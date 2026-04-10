5a60e3689c8667c63740a5c1ad4ae31dba6ccee1
Almighty42 <xenoman4202@gmail.com>
2026-04-10 16:51:11 +0200

    Finished basic layout of one of the weather dashboard views

--- app/blueprints/weather/__init__.py
+++ app/blueprints/weather/__init__.py
@@ -1,12 +1,15 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
+from app.data_sources.weather import fetch_weather_data
+
 weather_bp = Blueprint('weather_bp', __name__)
 
 @weather_bp.route('/', defaults={'page': 'weather'})
 @weather_bp.route('/<page>')
 def show(page):
+    print(fetch_weather_data(), flush=True)
     try:
-        return render_template(f'weather/{page}.html')
+        return render_template(f'weather/{page}.html', data=[])
     except TemplateNotFound:
         abort(404)
---
+++ app/data_sources/weather.py
@@ -0,0 +1,2 @@
+def fetch_weather_data():
+    return "Hello"
